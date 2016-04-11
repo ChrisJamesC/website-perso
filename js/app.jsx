be51297e212b0229a8211abe7668cf7add4a1d92
Christopher Chiche <christopher.chiche@gmail.com>
2016-04-11 19:50:41 +0200

    Fix typo in adopt a fan description

--- js/app.jsx
+++ js/app.jsx
@@ -75,7 +75,7 @@ const appData = {
             title: "Adopt a Fan", 
             source: "https://github.com/ChrisJamesC/AdoptAFanSWLausanne", 
             link: "http://chrisjamesc.github.io/AdoptAFanSWLausanne/", 
-            desc: "A proof of concept website built with a friend during a startup weekend (48h). The goal is to organize activities between fans around majar sport events locations."
+            desc: "A proof of concept website built with a friend during a startup weekend (48h). The goal is to organize activities between fans around major sport event locations."
         }, 
         {
             title: "React tutorial", 
